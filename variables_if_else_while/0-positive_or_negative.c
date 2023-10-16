db85774e6d993b0881c5fee87de469459ce02333
sachihiroo <saif3antar@gmail.com>
2023-10-16 04:00:15 -0700

    positive or negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - This is the entry point of the codemain. 
+ *description:If the expression is true (the value of
+ *the expression is not 0) then the
+ *block is executed.
+ *Return - 0 Successful
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0) 
+		printf("%d is positive", a);
+	else 
+	if (n < 0)
+		printf("%d is negative", n);
+	else
+	if (n=0)
+		printf("%d is zero", n);	
+	return (0);
+}
