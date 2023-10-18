736bb34dda3beb3da2a9e7e663dd6f1b1413b6db
sachihiroo <saif3antar@gmail.com>
2023-10-18 14:59:57 -0700

    print numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,12 +6,12 @@
 *  numbers of base 10
 *Return: Always (Success)
 */
-void main()
-{     
+int main(void)
+{
 int i;
-for (i=1;i<=10;i++)
-{      
-printf("%d ",i);
+for (i = 1; i <= 10; i++)
+{
+printf("%d ", i);
 }
 printf("\n");
 }
