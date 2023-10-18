a7fe216b8807f210bf79c34a05b9f508c1fb81ee
sachihiroo <saif3antar@gmail.com>
2023-10-18 13:17:44 -0700

    print alphabt

---
+++ variables_if_else_while/4-prnt_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point.
+*
+*Write a program that prints the alphabet in lowercase and uppercase.
+*
+*Return: Always (Success)
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'd' ; ch++)
+{
+putchar(ch);
+}
+for (ch = [f..p] ; ch <= [r..z] ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
