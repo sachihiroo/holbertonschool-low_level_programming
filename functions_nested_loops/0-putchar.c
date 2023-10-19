17057ad5279b739f4c8baba77398a274e29148d5
sachihiroo <saif3antar@gmail.com>
2023-10-19 07:13:39 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*a program that prints _putchar
+*Return: Always (Success)
+*/ 
+int main(void)
+{
+printf("_putchar");
+{
+putchar('\n');
+}
+return (0);
+}
