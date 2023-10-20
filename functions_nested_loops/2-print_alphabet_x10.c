04c32436b9dca81b303ec6a303c94f6417aca83b
sachihiroo <saif3antar@gmail.com>
2023-10-20 01:17:08 -0700

    alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet - check the code
+*
+* Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+char i;
+int x;
+
+for (x = 0; x < 10 ; ++x)
+for (i = 'a'; i <= 'z'; ++i)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
