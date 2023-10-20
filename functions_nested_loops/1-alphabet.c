ba47585f60f5d67ac6c017a16fcfe535dbdbfa55
sachihiroo <saif3antar@gmail.com>
2023-10-20 00:49:55 -0700

    alphabet lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio>
+#include "main.h"
+/**
+*main - Entry point
+*
+*a function that prints the alphabet, in lowercase
+*
+*Return: Always (Success)
+*/
+{
+void print_alphabet(void)
+char i;
+
+for (i = 'a' ; i <= 'z' ; ++i)
+{
+_putchar(i);
+putchar('\n');
+}
+}
