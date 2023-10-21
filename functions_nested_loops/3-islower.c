5c52d0f4104035f017b53adf18376ac56d1e33e2
sachihiroo <saif3antar@gmail.com>
2023-10-21 04:25:14 -0700

    lowercase checker

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* int_islower - check the code
+*
+* Return: Always 0.
+*/
+
+int _islower(int c);
+{
+int i;
+
+if (i >= 97) && (i <= 122)
+	return (1);
+else 
+	return (0);
+{
+_putchar(i);
+}
+_putchar('\n');
+}
