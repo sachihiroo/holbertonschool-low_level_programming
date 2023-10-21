e8a39eff280b474a69db8bed79762f81514cfb7d
sachihiroo <saif3antar@gmail.com>
2023-10-21 08:02:57 -0700

    9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * times_table - print 9.
+ */
+void times_table(void)
+{
+  int i;
+  int j;
+  int x;
+
+  for (i = 0; i < 10; i++)
+    {
+     for (j = 0; j < 10; j++)
+	{
+	  x = i * j;
+	  if (x == 0 && j == 0)
+	    {
+	      _putchar(x + '0');
+	    }
+	  else
+	    {
+	      if (x < 10)
+		{
+		  _putchar(',');
+		  _putchar(' ');
+		  _putchar(' ');
+		  _putchar(x + '0');
+		}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(x / 10 + '0');
+					_putchar(x % 10 + '0');
+			void times_table(void)	}
+			}
+		}
+		_putchar('\n');
+	}
+}
