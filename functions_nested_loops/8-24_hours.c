4b4e718eb360b2435ce9eb45e8cdf0b53d554349
sachihiroo <saif3antar@gmail.com>
2023-10-21 07:06:17 -0700

    8-24_hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - check the code
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int x,n;
+
+	for (x = 0; x < 24; ++x)
+	{
+	for (n = 0; n < 60; ++n)
+	{
+		_putchar (x / 10 + '0');
+		_putchar (x % 10 + '0');
+		_putchar (':');
+		_putchar (n / 10 + '0');
+		_putchar (n % 10 + '0');
+		_putchar ('\n');
+	}
+	}
+}
