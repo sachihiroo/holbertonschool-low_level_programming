f70210dc76da21c6679a09a3d2b89a0d9a4f2058
sachihiroo <saif3antar@gmail.com>
2023-10-22 14:56:47 -0700

    print a number

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - Entry Point
+ * @n: integer to be printed
+ * Return: 0 (Success)
+ */
+void print_number(int n)
+{
+	unsigned int num, div = 10;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+		num = n;
+
+	if (num / 10 > 0)
+		print_number(num / 10);
+	putchar(num % 10 + '0');
+}
