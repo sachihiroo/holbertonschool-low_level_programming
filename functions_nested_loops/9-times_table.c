05cc0ec5ba94aca88bf71ffa46a10a928f32a704
sachihiroo <saif3antar@gmail.com>
2023-10-22 05:24:08 -0700

    9-times_table.

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,41 +1,42 @@
 #include "main.h"
-/**
-*times_table - print 9.
-*/
-void times_table(void)
-{
-int i;
-int j;
-int x;
+/*more headers goes there*/
 
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-x = i * j;
-if (x == 0 && j == 0)
-{
-_putchar(x + '0');
-}
-else
-{
-if (x < 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(x + '0');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(x / 10 + '0');
-_putchar(x % 10 + '0');
+/**
+ * times_table - print 9.
+ */
 void times_table(void)
-}
-}
-}
-_putchar('\n');
-}
+{
+  int i;
+  int j;
+  int x;
+
+  for (i = 0; i < 10; i++)
+    {
+     for (j = 0; j < 10; j++)
+	{
+	  x = i * j;
+	  if (x == 0 && j == 0)
+	    {
+	      _putchar(x + '0');
+	    }
+	  else
+	    {
+	      if (x < 10)
+		{
+		  _putchar(',');
+		  _putchar(' ');
+		  _putchar(' ');
+		  _putchar(x + '0');
+		}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(x / 10 + '0');
+					_putchar(x % 10 + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
