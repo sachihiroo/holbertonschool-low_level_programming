058185680f0727410d6d13844ca8e04e02705f06
sachihiroo <saif3antar@gmail.com>
2023-10-22 13:52:36 -0700

    3-print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * print_numbers - Entry Point
+  * Return: 0 (Success)
+  */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
