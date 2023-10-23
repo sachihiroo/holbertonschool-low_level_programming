eaf93095261470390f48b3754cdc14d90ade7a99
sachihiroo <saif3antar@gmail.com>
2023-10-23 05:05:29 -0700

    task 3

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - check the code
+ * @s: char
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	s = "My first strlen!";
+	len = _strlen(s);
+}
