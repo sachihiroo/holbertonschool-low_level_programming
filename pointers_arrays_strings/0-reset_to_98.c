659da08c937bed8ea090493a5133ae9c245ab3a9
sachihiroo <saif3antar@gmail.com>
2023-10-23 04:33:46 -0700

    task 1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * reset_to_98 - check the code 
- *
+ * reset_to_98 - check the code
+ * *n: a pointer
  * Return: Always 0.
  */
 void reset_to_98(int *n)
