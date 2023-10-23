60a9186eeccb27c4b2e5dc5545ec74061748f32d
sachihiroo <saif3antar@gmail.com>
2023-10-23 04:50:11 -0700

    task 2

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,13 +1,12 @@
 /**
  * swap_int - check the code
- * @a,b: pointers
- * @n: int
+ * @a: pointer
+ * @b: pointer
+ * 
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
 {
-	int n;
-
 	n = *a;
 	*a = *b;
 	*b = n;
