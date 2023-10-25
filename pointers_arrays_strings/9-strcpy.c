3954d89d4ae1cfe057455d33423253ef6d6a660b
sachihiroo <saif3antar@gmail.com>
2023-10-25 11:05:57 -0700

    task 10

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcpy - check the code
+ * @src
+ * @dest: pointer
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, n;
+
+	n = strlen(str);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
