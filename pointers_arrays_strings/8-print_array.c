824b3a90203ecbad9d7b546bcf5a32cd307cc592
sachihiroo <saif3antar@gmail.com>
2023-10-25 07:46:05 -0700

    task 9

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - check the code
+ * @str: pointer
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	*a = n;
+	for (int i =0; i < n; i++);
+	{
+	printf("%d, ", *(a + i));
+	}
+}
