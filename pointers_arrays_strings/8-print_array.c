05b04a77ccc66d89a572d78104394ec936c3f9e8
sachihiroo <saif3antar@gmail.com>
2023-10-25 10:16:56 -0700

    task 9

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,14 +3,14 @@
 #include <string.h>
 /**
  * print_array - check the code
- * @str: pointer
+ * @a: pointer
  * Return: Always 0.
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i =0; i < n; i++);
+	for (i =0; i < n; i++)
 	{
 		printf("%d, ", *(a + i));
 		if (i < n -1)
