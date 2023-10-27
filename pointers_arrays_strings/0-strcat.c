3bc10753ad9f71b3679006ff6db4e71d90c2a841
sachihiroo <saif3antar@gmail.com>
2023-10-27 06:16:49 -0700

    task 12

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	a = 0;
 	while (dest[a])
 		a++;
-	for (b = 0; src[b] ; b++;)
-		dest[a++] = src[b]
+	for (b = 0; src[b] ; b++)
+		dest[a++] = src[b];
 			return (dest);
 }
