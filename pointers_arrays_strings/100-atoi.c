bec7d8b3483372fb78ee4d2a8cbf8e2f337de77e
sachihiroo <saif3antar@gmail.com>
2023-10-27 01:22:52 -0700

    task 11

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+/**
+ * _atoi - check the code
+ * @s: pointer
+ * Return: Always 0.
+ */
 int _atoi(char *s)
 {
 	unsigned int num = 0;
 	int sign = 1;
 
-	do
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
 		else if ((*s >= '0') && (*s <= '9'))
