63f0a40e36a7a9a94c6655692da20998bedfdf13
sachihiroo <saif3antar@gmail.com>
2023-10-27 06:26:24 -0700

    task 13

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strncat - check the code
+ * @src: pointer
+ * @dest: pointer
+ * @n: int
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (i = 0; i < n && src[i] != '\0' ; i++)
+		dest[n + i] = src[i];
+	dest[c + i] = '\0'
+			return (dest);
+}
