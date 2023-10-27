aecbae7545a39cff7e5c8f351adecbd4df899e2e
sachihiroo <saif3antar@gmail.com>
2023-10-27 05:33:57 -0700

    task 12

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcat - check the code
+ * @src: pointer
+ * @dest: pointer
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int n, i;
+
+	n = strlen(src);
+	for (i = 0; i < n && src[i] != ; i++)
+	{
+		dest[n + i] = src[i];
+		dest[n + i] = '\0';
+		return (dest);
+}
