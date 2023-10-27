efca701dc8e7fe9fd0276936e73dd2dd0c40fa9d
sachihiroo <saif3antar@gmail.com>
2023-10-27 06:49:01 -0700

    task 14

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strncat - check the code
+ * @src: pointer
+ * @dest: pointer
+ * @n: int
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	 int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
