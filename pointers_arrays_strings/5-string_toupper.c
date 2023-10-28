2cb3e8cf37eb376221eb15496243b5fab0136004
sachihiroo <saif3antar@gmail.com>
2023-10-28 09:35:02 -0700

    task 16

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *string_toupper - check the code
+ * @*str: pointer
+ * Return: Always 0.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i =0; str[i] != '\0';i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
