0febc35e370c891227bb700c6b3a054236d7d15b
sachihiroo <saif3antar@gmail.com>
2023-10-28 07:56:05 -0700

    task 15

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, x = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0' && x == 0;)
+	while (s1[i] != '\0' && s2[i] != '\0' && x == 0)
 		x = s1[i] - s2[i];
 	{
 		i++;
