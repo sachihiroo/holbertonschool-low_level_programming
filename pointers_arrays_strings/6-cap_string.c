db5aeeef02ae53fca98d755169bbfb2383919425
sachihiroo <saif3antar@gmail.com>
2023-10-29 06:20:58 -0700

    task 17

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *cap_string - check the code
+ * @src: pointer
+ * @dest: pointer
+ * @n: int
+ * Return: Always 0.
+ */
+char *cap_string(char *)
+{
+	int i, cap, l = 0;
+
+	while (*(s + l))
+		l++;
+	if (s[0] <= 'z' && s[0] >= 'a')
+		s[0] -= 32;
+	for (i = 0; i < l; i++)
+	{
+		if (s[i] == '\t' || s[i] == '\n')
+			cap = 1;
+		else if (s[i] == ' ' || s[i] == '.')
+			cap = 1;
+		else if (s[i] == ',' || s[i] == ';')
+			cap = 1;
+		else if (s[i] == '!' || s[i] == '?')
+			cap = 1;
+		else if (s[i] == '(' || s[i] == ')')
+			cap = 1;
+		else if (s[i] == '{' || s[i] == '}')
+			cap = 1;
+		else if (s[i] <= 'z' && s[i] >= 'a' && cap != 0)
+		{
+			s[i] -= 32;
+			cap = 0;
+		}
+		else
+			cap = 0;
+	}
+	return (s);
+}
