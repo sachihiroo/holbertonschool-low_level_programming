4844e95e7096efa60320fb7edd4039ebe2e2ed52
sachihiroo <saif3antar@gmail.com>
2023-10-29 15:38:39 -0700

    task 21

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strspn - check the code
+ * @s : string
+ * @accept : string 
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int j, i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != s[i]; j++)
+			if (accept[j] == '\0')
+				return (i);
+	return (i);
+}
