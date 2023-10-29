2ae508201692c47a321546e98e8652539dbca733
sachihiroo <saif3antar@gmail.com>
2023-10-29 15:09:08 -0700

    task 18

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 /**
-* *leet - check the code.
+* *leet - encodes a string.
 * @s: pointer
 * Return: Always 0.
 */
 char *leet(char *s)
 {
-	int i;
-	char x[5] = {'A', 'E', 'O', 'T', 'L'};
-	int n[5] = {4, 0, 3, 7, 1};
+	unsigned int i;
+	char x[] = {'A', 'E', 'O', 'T', 'L'};
+	int n[] = {4, 0, 3, 7, 1};
 
 	while (*s)
 	{
-		for (i = 0; i < x; i++)
+		for (i = 0; i < sizeof(x) / sizeof(char); i++)
 		{
-			if (*s == x[i] || *s == + 32)
+			if (*s == x[i] || *s == x[i] + 32)
 			{
 				*s = 48 + n[i];
 			}
