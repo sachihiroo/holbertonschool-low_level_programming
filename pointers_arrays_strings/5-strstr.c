01980267ceeb6ffa924bea6b4e9d77b7fcdb4b82
sachihiroo <saif3antar@gmail.com>
2023-10-29 15:45:27 -0700

    task 23

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strstr - check the code
+ * Description: locate the substring
+ * @haystack : the string to be searched in
+ * @needle : the substring to search for
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, n = 0;
+
+	while (needle[n] != '\0')
+		n++;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != n)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (NULL);
+}
