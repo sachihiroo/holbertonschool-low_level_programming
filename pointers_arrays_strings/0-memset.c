245e1a7a8f7b25f21a9d55c4e47141a20fca4c2e
sachihiroo <saif3antar@gmail.com>
2023-10-29 15:31:44 -0700

    task 18

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _memset - check the code
+ * Description: fill memory with a constant byte
+ * @s : pointer
+ * @b : charachter to be copied
+ * @n : number of bytes
+ * Return: char*
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
