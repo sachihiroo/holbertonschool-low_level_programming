4032dfd1a1058069feaab2a54edfe7e7959f36d9
sachihiroo <saif3antar@gmail.com>
2023-10-30 02:10:25 -0700

    task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts_recursion - check the code.
+ * @s: pointer
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	int n;
+
+	for (*s != '\0'; s++)
+	{
+		return;
+	}
+	return (0);
+}
