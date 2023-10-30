8a21c708a1345eee3237149ac0a2f611ecdb1b34
sachihiroo <saif3antar@gmail.com>
2023-10-30 02:55:44 -0700

    task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _print_rev_recursion - check the code
+ * @s: pointer
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s*);
+	}
+}
