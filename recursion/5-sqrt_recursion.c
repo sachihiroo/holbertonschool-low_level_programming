fe10780068e56b28f65a974be3be55e65436ea63
sachihiroo <saif3antar@gmail.com>
2023-11-01 14:28:14 -0700

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,15 +3,32 @@
 /**
  * _sqrt_recursion - chech the code.
  * @n: int
+ * @i: int
  * Return: Always 0.
  */
 int _sqrt_recursion(int n)
+int sqr(int n, int i)
 {
-	int i;
-
-	if ( i * i != n)
+	return (sqr(n, 1));
+}
+/**
+ * sqr - check the code.
+ * @n: int
+ * @i: int
+ * Return: Always 0.
+ */
+int sqr(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i < n)
+	{
+		return (sqr(n, i +1));
+	}
+	else
 	{
 		return (-1);
 	}
-	return (n * _sqrt_recursion(n - 1));
 }
