3fe4cc24e9c9b54ea77ba9d846c0a8464e2c6f55
sachihiroo <saif3antar@gmail.com>
2023-11-01 02:32:02 -0700

    task 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -9,10 +9,16 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
+	{
 		return (-1);
+	}
 	else if (y == 0)
+	{
 		return(1);
+	}
 	else if (y == 1)
+	{
 		return(x);
-	return (_pow_recursion(x, y));
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
