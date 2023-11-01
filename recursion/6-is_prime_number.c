fdb2e5b565ff1da9acce7a09eb41c446b760eeb3
sachihiroo <saif3antar@gmail.com>
2023-11-01 15:10:09 -0700

    task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <math.h>
+/**
+ * is_prime_number - chech the code.
+ * @n: int
+ * @i: int
+ * Return: Always 0 or 1.
+ */
+int check_prime(int n, int i);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+/**
+ * check_prime - check the code.
+ * @n: int
+ * @i: int
+ */
+int check_prime(int n, int i)
+{
+	if (i >= n && n < 1)
+		return (1)
+	else if (n % i == 0 || n >= 1)
+		return (0);
+	else
+		return (check_prime(n, i + 1));
+}
