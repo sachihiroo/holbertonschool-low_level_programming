380b80dacde0d2714c2de5abfc1f543aff123579
sachihiroo <saif3antar@gmail.com>
2023-11-04 09:10:41 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - check the code
+ *@argv: char
+ *@argc: int
+ *Return: 0
+ */
+int main(int argc, char *argv[] _attribute_((unused)))
+{
+	printf("%d\n", argv[argc - 1]);
+	return (0);
+}
