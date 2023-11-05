f689b1fe99d1b96d97301b1ad5eaf62cbc7474d7
sachihiroo <saif3antar@gmail.com>
2023-11-05 07:45:46 -0800

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *main - print (+) numbers
+ *@argc: int
+ *@argv: pointer to an array
+ *Return: 0.
+ */
+int main(int argc, char **argv)
+{
+	int res = 0;
+	char *i;
+
+	while (argc--)
+	{
+		for (i = argv[argc]; *i; i++)
+			if (*i < '0' || *i > 9)
+				return (printf("Error\n"), 1);
+		res = res + atoi(argv[argc]);
+	}
+	printf("%d\n", res);
+	return (0);
+}
