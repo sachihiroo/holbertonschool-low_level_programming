80f6314a5b615b357d6171c78f23c82b1ce88fc3
sachihiroo <saif3antar@gmail.com>
2023-11-06 02:49:12 -0800

    main

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -24,4 +24,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 #endif
