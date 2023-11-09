82046026ebdd918226ef099f44eccc5bd7ff3efd
sachihiroo <saif3antar@gmail.com>
2023-11-09 18:26:56 +0100

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,25 +7,25 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int n1, n2;
-    char *c;
+	int n1, n2;
+	char *c;
 
-    n1 = strlen(s1);
-    n2 = strlen(s2);
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-    c = malloc(n1 + n2 + 1);
-    if (c == NULL)
-    {
-        return (NULL);
-    }
-    strcpy(c, s1);
-    strcat(c, s2);
-    return (c);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	n1 = strlen(s1);
+	n2 = strlen(s2);
+	c = malloc(n1 + n2 + 1);
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(c, s1);
+	strcat(c, s2);
+	return (c);
 }
