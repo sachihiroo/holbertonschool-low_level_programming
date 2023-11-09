942aa5a21d75f843f655791e8860fd854c48420c
sachihiroo <saif3antar@gmail.com>
2023-11-09 10:58:22 +0100

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include « main.h »
+/**
+ * _strdup - returns a pointer 
+ * @str: pointer
+ * Return: NULL or string
+ */
+char *_strdup(char *str)
+{
+    int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+    {
+        i++;
+    }
+    printf("%s, str");
+    str = malloc(sizeof(char) * i + 1);
+    return (str);
+}
