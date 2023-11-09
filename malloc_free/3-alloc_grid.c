be4ca46a2a867d9c89d57c8c5a13eb1cab11b2c8
sachihiroo <saif3antar@gmail.com>
2023-11-09 11:43:40 +0100

    task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	char **array;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL)
+	}
+	array = mallac(sizeof(int *) * height);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int *) * width);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array);
+}
