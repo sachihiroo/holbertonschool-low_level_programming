3aa10933407cb2ab2f770c578bf359f26910b4e8
sachihiroo <saif3antar@gmail.com>
2023-11-12 19:17:38 +0100

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,29 +8,29 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int n1, n2;
-    char *p;
-
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-    n1 = strlen(s1);
-    n2 = strlen(s2);
-    if (n >= n2)
-    {
-        n = n2;
-    }
-    p = malloc(sizeof(char) * n1 + n2 + 1);
+	unsigned int n1, n2;
+	char *p;
+	
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	n1 = strlen(s1);
+	n2 = strlen(s2);
+	if (n >= n2)
+	{
+		n = n2;
+	}
+	p = malloc(sizeof(char) * n1 + n2 + 1);
 	if (p == NULL)
-    {
-        return (NULL);
-    }
-    strcpy(p, s1);
-    strncat(p, s2, n);
-    return (p);
+	{
+		return (NULL);
+	}
+	strncpy(p, s1);
+	strncat(p, s2, n);
+	return (p);
 }
