17bb6693a4bcae5a43fc9561b2dfa483e56be591
sachihiroo <saif3antar@gmail.com>
2023-11-12 19:56:49 +0100

    task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_calloc - a function that concatenates two strings.
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: pointer oor NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = calloc(size * nmemb);
+	if (p == NULL)
+	{
+		return (NULL)
+	}
+	return (p);
+}
