cc632d6f6b2c51499e4dab01d7a8900160308de0
sachihiroo <saif3antar@gmail.com>
2023-11-12 19:26:10 +0100

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int n1, n2;
 	char *p;
-	
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = n2;
 	}
-	p = malloc(sizeof(char) * (n1 + n2 + 1));
+	p = malloc(sizeof(char) * (n1 + n + 1));
 	if (p == NULL)
 	{
 		return (NULL);
