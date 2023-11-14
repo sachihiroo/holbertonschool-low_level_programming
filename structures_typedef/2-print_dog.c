12f042c7e52ca2a80dc65d61a820fe8d0b41f191
sachihiroo <saif3antar@gmail.com>
2023-11-14 10:10:12 +0100

    task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+/**
+ * init_dog - Short description
+ * @name: char
+ * @age: float
+ * @owner: char
+ * @d: the dog to init
+ * Return: void.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("nil");
+	}
+	if (d->name == NULL)
+	{
+		d->name = "nil";
+		printf("%s", d->name);
+	}
+}
