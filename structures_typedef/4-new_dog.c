a28e3ae9a7e4efd82858ea6ae909cc0a81c54490
sachihiroo <saif3antar@gmail.com>
2023-11-15 10:17:25 +0100

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,34 +6,35 @@
  * @owner: char
  *
  *  Return: 0 or NULL.
- * 
+ *
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
+	char dog_t *d;
+	int n1, n2;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
-		return;
+		return (NULL);
 	}
 	n1 = strlen(name);
-	n2 = strlen(owner);
-	d->name = malloc(sizeof(char) * (n1 +1);
+	d->name = malloc(sizeof(char) * (n1 + 1));
 	if (d->name == NULL)
 	{
 		free(d);
-		return;
+		return (NULL);
 	}
+	n2 = strlen(owner);
 	d->owner = malloc(sizeof(char) * (n2 + 1));
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
-		return;
+		return (NULL);
 	}
 	strcpy(d, name, name);
 	d->age = age;
 	strcpy(d, owner, owner);
-	return;
+	return (d);
 }
