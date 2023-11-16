8f3a102580415348a64f455bf383c94ccd90c129
sachihiroo <saif3antar@gmail.com>
2023-11-16 10:08:41 +0100

    task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 /**
  * array_iterator - a function that prints a name.
- * @name: char
- * @f: void
- *
+ * @array: int
+ * @size: size_t
+ * @action: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
