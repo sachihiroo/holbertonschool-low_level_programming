53206e2608bae49b0c54a731239c58be233ddec2
sachihiroo <saif3antar@gmail.com>
2023-11-18 15:58:53 +0100

    Function selection

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+/**
+ * *get_op_func -
+ * @s: char
+ * Return: int
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (strcmp(s, ops[i].op))
+		{
+			return ((*f));
+		}
+		i++;
+	}
+	return (NULL);
+}
