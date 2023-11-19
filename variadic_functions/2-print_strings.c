8ee6ed0955cbbeff1e1773ea2f6975e8358f14dd
sachihiroo <saif3antar@gmail.com>
2023-11-19 18:35:15 +0100

    task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 		}
 	}
-	va_end(args);
 	printf("\n");
 	s = va_arg(args, char *);
 	if (s == NULL)
@@ -30,4 +29,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("\n");
 		return;
 	}
+	va_end(args);
 }
