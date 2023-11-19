27c23760fc37eece7301fb1cb9f371837d0b9af0
sachihiroo <saif3antar@gmail.com>
2023-11-19 15:33:18 +0100

    task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,20 +2,20 @@
 /**
  * sum_them_all - Check the code.
  * @n: const unsigned int
- * 
+ *
  * Return: 0.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i, sum = 0;
+	va_list args;
+	unsigned int i, sum = 0;
 
-    va_start (args, n);
-    
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg (args, int);
-        va_end(args);
-    }
-    return (sum);
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+		va_end(args);
+	}
+	return (sum);
 }
