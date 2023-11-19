bf57eec4b6f30c4faaffc2c4586c44e77d3f012b
sachihiroo <saif3antar@gmail.com>
2023-11-19 16:07:06 +0100

    task 2

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,24 +1,24 @@
 #include "variadic_functions.h"
 /**
- * sum_them_all - Check the code.
+ * print_numbers - Check the code.
  * @n: const unsigned int
- *
- * Return: 0.
+ * @separator: const char pointer
+ * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
+	va_list args;
+	unsigned int i;
 
-    va_start (args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(args,int));
-        if ((separator != NULL) & (i < n - 1))
-        {
-                printf("%s\n", separator);
-        }
-    }
-    va_end(args);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if ((separator != NULL) & (i < n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
 }
