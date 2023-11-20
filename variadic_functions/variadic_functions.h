0ad85617e2c11a37eaf2ce3a4f3f927069e63857
sachihiroo <saif3antar@gmail.com>
2023-11-20 13:23:26 +0100

    Header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,9 +4,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct om
+{
+	char op;
+	void (*take)(va_list *);
+} om_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 char *strings(char *s);
+
+
+void om_char(va_list args *);
+void om_int(va_list args *);
+void om_float(va_list args *);
+void om_string(va_list args *);
 #endif
