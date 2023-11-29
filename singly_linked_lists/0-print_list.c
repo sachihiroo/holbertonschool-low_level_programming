74effc3138601c6988c571c5d01e077e992599fb
sachihiroo <saif3antar@gmail.com>
2023-11-29 15:50:30 +0100

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - a function that prints all the elements of a list.
+ * @h: pointer
+ *
+ * Return: 0.
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+			printf("[%d] %s", h->len, h->str);
+		i += 1;
+		h = h->next;
+	}
+	return (i);
+}
