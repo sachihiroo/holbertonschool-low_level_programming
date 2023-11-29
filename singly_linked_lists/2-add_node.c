c0b3a4ce31c15c89cc435c71ec03307498a5776f
sachihiroo <saif3antar@gmail.com>
2023-11-29 16:48:47 +0100

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 /**
  * *add_node - a function that adds a new node at the beginning of a list.
  * @str: const char
- * @head:
+ * @head: pointer
  * Return: 0.
  */
 list_t *add_node(list_t **head, const char *str)
@@ -11,12 +11,21 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (second_node == NULL)
 	{
+		free (second_node);
 		return (NULL);
 	}
 
 	second_node->str = strdup(str);
+
+	if (second_node->str == NULL)
+	{
+		free (second_node);
+		return (NULL);
+	}
+
 	second_node->len = strlen(second_node->str);
 	second_node->next = *head;
 	*head = second_node;
+	
 	return (second_node);
 }
