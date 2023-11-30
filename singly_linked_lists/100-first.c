61515d032b90d2794894aaf4f5db4ebdfad5302f
sachihiroo <saif3antar@gmail.com>
2023-11-30 10:57:12 +0100

    task 100

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -7,4 +7,5 @@ void race(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
