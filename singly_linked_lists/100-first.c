a3cf0c8045b04fd01ddcbf8d2ad18fdde891abbb
sachihiroo <saif3antar@gmail.com>
2023-11-30 11:00:50 +0100

    task 100

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -3,9 +3,9 @@
  * race - printing a line before main function
  * Return: void.
  */
+void race (void) __attribute__ ((constructor))
 void race(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
