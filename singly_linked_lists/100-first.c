4adb66ee89d2bc38a405d25b5fc8668558b1c04e
sachihiroo <saif3antar@gmail.com>
2023-11-30 10:51:33 +0100

    task 100

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * race - printing a line before main function
+ * Return: void.
+ */
+void race(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
