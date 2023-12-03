3ceacb5573066f4e7bfeb87763992a8e251b58a5
sachihiroo <saif3antar@gmail.com>
2023-12-03 15:29:32 +0100

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list -  a function that frees a list.
+ * @head: pointer 
+ */
+void free_list(list_t *head)
+{
+	list_t *clear;
+	while (head != NULL)
+	{
+		clear = head;
+		head = head->next;
+		free (clear->str);
+		free (clear);
+	}
+}
