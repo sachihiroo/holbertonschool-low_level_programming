bae30f7e4f619b8d1abaa88780bcc2a72651c05a
sachihiroo <saif3antar@gmail.com>
2023-12-03 15:29:15 +0100

    Header file

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -25,4 +25,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void race (void) __attribute__ ((constructor));
 void race(void);
+void free_list(list_t *head);
 #endif
