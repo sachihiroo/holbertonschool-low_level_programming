6a9702ef9001242a94399622fa1d9c2f67b123c9
sachihiroo <saif3antar@gmail.com>
2023-12-03 15:24:20 +0100

    task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,7 +37,7 @@ list_t *add_node_end(list_t **head, const char *str)
 			third_node = third_node->next;
 		}
 
-		third_node = second_node;
+		third_node->next = second_node;
 	}
 
 	return (second_node);
