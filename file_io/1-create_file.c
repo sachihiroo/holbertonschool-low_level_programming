850d88afe5f5a50983439502aa7678da07e87436
sachihiroo <saif3antar@gmail.com>
2023-12-10 19:37:24 +0100

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		n = 0;
 	}
 	else
-	n =strlen(text_content);
+		n = strlen(text_content);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
