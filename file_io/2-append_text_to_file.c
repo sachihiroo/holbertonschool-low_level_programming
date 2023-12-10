8fb16122c572c39a12bc1f30359c2c04ff071a95
sachihiroo <saif3antar@gmail.com>
2023-12-10 20:10:44 +0100

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * create_file - Create a function that creates a file.
+ * append_text_to_file - a function that appends text at the end
  * @filename: char
  * @text_content: text
  * Return: 0.
@@ -10,24 +10,24 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t n;
 
-    if (filename == NULL)
-        return -1;
+	if (filename == NULL)
+		return -1;
 
-    fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    if (fd == -1)
-        return -1;
+	if (fd == -1)
+		return -1;
 
-    if (text_content != NULL)
-    {
-    	n = write(fd, text_content, strlen(text_content));
-        if (n == -1)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+	if (text_content != NULL)
+	{
+		n = write(fd, text_content, strlen(text_content));
+		if (n == -1)
+		{
+			close(fd);
+			return -1;
+		}
+	}
 
-    close(fd);
-    return 1;
+	close(fd);
+	return 1;
 }
