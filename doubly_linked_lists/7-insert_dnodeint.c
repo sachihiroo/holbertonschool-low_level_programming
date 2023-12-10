c960d7cd1ee16a32d3f4be372b75133cca37e501
sachihiroo <saif3antar@gmail.com>
2023-12-10 15:11:34 +0100

    task 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *curr = *h;
+	dlistint_t *new unsigned int i;
+
+	if (!h)
+	{
+		return (NULL);
+	}
+	if (*h == NULL && idx != 0)
+	{
+		return (NULL);
+	}
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	if (idx == 0)
+	{
+		if (*h)
+		{
+			curr->prev = new;
+		}
+		*h = new;
+		new->prev = NULL;
+		new->next = curr;
+		return (new);
+	}
+	for (i = 1; i < idx; i++)
+	{
+		curr = curr->next;
+		if (curr == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->prev = curr;
+	new->next = curr->next;
+	if (curr->next)
+	{
+		curr->next->prev = new;
+	}
+	curr->next = new;
+	return (new);
+}
