3d43c415b29aa5abff2262df967d7ce2812db98d
sachihiroo <saif3antar@gmail.com>
2023-12-10 19:36:01 +0100

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - Create a function that creates a file.
+ * @filename: char
+ * @text_content: text
+ * Return: 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, n;
+
+	if (text_content == NULL)
+	{
+		n = 0;
+	}
+	else
+	n =strlen(text_content);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	if (fd == -1)
+		return (-1);
+
+	write(fd, text_content, n);
+	close(fd);
+
+	return (1);
+}
