4d19311fb32b56db1a43735804e386164f28a95a
sachihiroo <saif3antar@gmail.com>
2023-12-10 20:12:09 +0100

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,12 +11,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t n;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -24,10 +24,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (n == -1)
 		{
 			close(fd);
-			return -1;
+			return (-1);
 		}
 	}
 
 	close(fd);
-	return 1;
+	return (1);
 }
